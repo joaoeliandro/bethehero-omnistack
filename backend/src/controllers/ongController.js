64093e4972d717c1dc5072e02f33dea9f543e156
João Eliandro Germano <52173525+whistlerip@users.noreply.github.com>
2020-03-26 03:26:47 -0300

    Alteração no nome da classe

--- backend/src/controllers/ongController.js
+++ backend/src/controllers/ongController.js
@@ -4,7 +4,7 @@ const connection = require('../database/connection');
 module.exports = {
     async index(request, response) {
         const ongs = await connection('ongs').select('*');
-        
+
         return response.json(ongs);
     },
 
@@ -24,4 +24,4 @@ module.exports = {
 
         return response.json({ id });
     }
-}
+};
