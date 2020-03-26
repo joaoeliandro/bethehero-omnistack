73f551476709131d8e2f02190af1e2f31db1c29d
João Eliandro Germano <52173525+whistlerip@users.noreply.github.com>
2020-03-26 10:49:06 -0300

    Adicionado o método delete

--- backend/src/controllers/IncidentsController.js
+++ backend/src/controllers/IncidentsController.js
@@ -1,7 +1,12 @@
-const crypto = require('crypto');
 const connection = require('../database/connection');
 
 module.exports = {
+    async index(request, response) {
+        const incidents = await connection('incidents').select('*');
+
+        return response.json(incidents);
+    },
+
     async create(request, response) {
         const { titulo, descricao, valor } = request.body;
         const ong_id = request.headers.authorization;
@@ -15,5 +20,23 @@ module.exports = {
 
         // const id = result[0];
         return response.json({ id });
+    },
+
+    async delete(request, response) {
+        const { id } = request.params;
+        const ong_id = request.headers.authorization;
+
+        const incident = await connection('incidents')
+            .where('id', id)
+            .select('ong_id')
+            .first();
+
+        if (incident.ong_id !== ong_id) {
+            return response.status(401).json({ error: "operação não permitida" });
+        }
+
+        await connection('incidents').where('id', id).delete();
+
+        return response.status(204).send();
     }
 };
