d4b74b69ac48f0227d6624dd48609efed7d1eb5d
João Eliandro Germano <52173525+whistlerip@users.noreply.github.com>
2020-03-29 03:55:53 -0300

    Refatoração e adicionado tratamento erros valid

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
@@ -20,5 +21,6 @@ app.use(express.json());
  * Request Body: corpo da requisição, utilizado para criar ou alterar recursos
  */
 app.use(routes);
+app.use(errors());
 
-app.listen(3333);
+module.exports = app;
