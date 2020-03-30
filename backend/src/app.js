874e32eaf674b802fe16c32e88a6265666ff51da
João Eliandro Germano <52173525+whistlerip@users.noreply.github.com>
2020-03-30 18:29:08 -0300

    Retirado comentarios

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,20 +6,7 @@ const routes = require('./routes');
 const app = express();
 
 app.use(cors());
-// app.use(cors({
-//     origin: 'http://joaobetehero.heroku.com'
-// }));
 app.use(express.json());
-/**
- * Tipos de parametros
- * 
- * Query Params: parametros nomeados enviados na rota após "?"
- *        serve para filtros, paginação
- * 
- * Route Params: paramentros utilizados para identificar recursos
- * 
- * Request Body: corpo da requisição, utilizado para criar ou alterar recursos
- */
 app.use(routes);
 app.use(errors());
 
