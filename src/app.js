1dd2805bc66d18e7dd37701183cab12f20a3af7d
João Eliandro Germano <52173525+whistlerip@users.noreply.github.com>
2020-03-31 02:38:50 -0300

    Consertando o bug em prod

--- src/app.js
+++ src/app.js
@@ -5,7 +5,10 @@ const routes = require('./routes');
 
 const app = express();
 
-app.use(cors({origin: 'https://betheheroeliandro.herokuapp.com'}));
+app.use(cors({
+    origin: ['https://betheheroeliandro.herokuapp.com'],
+    exposedHeaders: ['Access-Control-Allow-Origin'],
+}));
 app.use(express.json());
 app.use(routes);
 app.use(errors());
