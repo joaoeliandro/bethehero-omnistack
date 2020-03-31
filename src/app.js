df55ebd432f773afe8eafa66b468df8d3f231124
João Eliandro Germano <52173525+whistlerip@users.noreply.github.com>
2020-03-31 17:01:33 -0300

    Att

--- src/app.js
+++ src/app.js
@@ -6,12 +6,6 @@ const routes = require('./routes');
 const app = express();
 
 app.use(express.json());
-app.options('*', cors({
-    origin: ['http://betheheroeliandro.herokuapp.com'],
-    methods: ['GET', 'PUT', 'POST', 'DELETE'],
-    exposedHeaders: ['Access-Control-Allow-Origin'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-}));
 app.use(cors());
 app.use(routes);
 app.use(errors());
