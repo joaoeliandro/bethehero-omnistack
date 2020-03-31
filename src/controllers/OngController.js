a11a2bd8e42efd1f1ba4cc797c2a2fa1072df8f5
João Eliandro Germano <52173525+whistlerip@users.noreply.github.com>
2020-03-31 00:53:01 -0300

    Alteração para bug em prod

---
+++ src/controllers/OngController.js
@@ -0,0 +1,27 @@
+const connection = require('../database/connection');
+const generateIDUtils = require('../utils/generateIDUtils');
+
+module.exports = {
+    async index(request, response) {
+        const ongs = await connection('ongs').select('*');
+
+        return response.json(ongs);
+    },
+
+    async create(request, response) {
+        const { nome, email, whatsapp, cidade, uf } = request.body;
+
+        const id = generateIDUtils();
+
+        await connection('ongs').insert({
+            id,
+            nome,
+            email,
+            whatsapp,
+            cidade,
+            uf
+        });
+
+        return response.json({ id });
+    }
+};
