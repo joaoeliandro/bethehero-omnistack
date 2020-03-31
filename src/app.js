beae8f3d7070808f50545e3cc7d19c2a1c2fabb6
João Eliandro Germano <52173525+whistlerip@users.noreply.github.com>
2020-03-31 04:22:12 -0300

    Att

--- src/app.js
+++ src/app.js
@@ -8,9 +8,8 @@ const app = express();
 app.use(cors({
     origin: ['http://betheheroeliandro.herokuapp.com'],
     methods: ['GET', 'PUT', 'POST', 'DELETE'],
-    exposedHeaders: ['Access-Control-Allow-Origin', 'Access-Control-Allow-Methods'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Custom-Header'],
-    credentials: true
+    exposedHeaders: ['Access-Control-Allow-Origin', 'Access-Control-Allow-Methods', 'Access-Control-Allow-Headers'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 app.use(express.json());
 app.use(routes);
