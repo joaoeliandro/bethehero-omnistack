88cfe61cd06417ea3460f76f807b59c4a5715222
João Eliandro Germano <52173525+whistlerip@users.noreply.github.com>
2020-03-31 17:17:14 -0300

    Atualizando para deploy

--- src/app.js
+++ src/app.js
@@ -6,12 +6,7 @@ const routes = require('./routes');
 const app = express();
 
 app.use(express.json());
-app.use(app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
-    next()
-}));
+app.use(cors());
 app.use(routes);
 app.use(errors());
 
